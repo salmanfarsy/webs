9533c88ba9bab9531d4c85d2f1206615c93d9477
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-07-08 14:02:45 +0600

    home route

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const app = express();
 
+//routes
+app.get('/', (req, res)=>{
+    res.send('hello to my new portfolio site')
+})
+
 
 //port
 app.listen(3000, ()=>{
